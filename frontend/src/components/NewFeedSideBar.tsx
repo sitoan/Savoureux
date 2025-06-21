8bcee64a1fc8f28b42faa4c84e2901a3f2fa3709
VinhQuangPham <pmwangvinh@gmail.com>
2025-06-21 12:11:34 +0700

    update styles

--- frontend/src/components/NewFeedSideBar.tsx
+++ frontend/src/components/NewFeedSideBar.tsx
@@ -1,5 +1,7 @@
+import "../styles/newFeedSideBar.css";
+
 const NewFeedSideBar = () => {
-  return <div></div>;
+  return <div id="new-feed-sidebar-container"></div>;
 };
 
 export default NewFeedSideBar;
---
+++ frontend/src/components/RecipeCard.tsx
@@ -0,0 +1,42 @@
+import "../styles/recipeCard.css";
+import shareIcon from "../assets/iconImages/share.png";
+import star from "../assets/iconImages/star.png";
+
+interface CardInfor {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  avg_rating: number;
+  onClick?: () => void;
+}
+const RecipeCard = ({
+  id,
+  title,
+  image,
+  description,
+  avg_rating,
+  onClick,
+}: CardInfor) => {
+  return (
+    <div className="recipe-card" onClick={onClick}>
+      <img src={image} alt={title} />
+      <h5>{title}</h5>
+      <div className="recipe-card-description">
+        <p>{description}</p>
+      </div>
+      <div className="recipe-card-footer">
+        <div className="rating">
+          <p>{avg_rating}</p>
+          <img src={star} alt="" />
+        </div>
+        <div className="share-button-area">
+          <button>
+            <img src={shareIcon} alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default RecipeCard;
