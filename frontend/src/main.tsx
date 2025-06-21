d5f443b7e07c1d13254e6b6f58e7424fd0da5773
VinhQuangPham <pmwangvinh@gmail.com>
2025-06-21 13:24:51 +0700

    update routing in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import HomePage from "./pages/HomePage";
+import RecipePage from "./pages/RecipePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/recipe/:id" element={<RecipePage />} />
+    </Routes>
+  </BrowserRouter>
+  // </StrictMode>
 );
